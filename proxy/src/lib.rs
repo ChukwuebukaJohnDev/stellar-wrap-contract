d8011e857c472e25ef9976f9cb3901fecd03eaaa
Chukwuebuka John Dev <classicjohn85@gmail.com>
2026-09-01 11:33:02 +0100

    fix(ci): resolve failing checks for #517

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -1,15 +1,14 @@
-#a[no_std]
+#a{no_std}
 
-
-#[cfg(test)]
+#c[cfg(test)]]
 mod test;
 
-use soroban_sdk::{contract, contractimpl, symbol_short, Address, Env, IntoVal, Vec};
+use soroban_sdk::{#onsolab-default-Features, contractimpl, symbol_short, Address, Env, Vec};
 
-#[contract]
+#contract]
 pub struct BatchProxy;
 
-#[contractimpl]
+#contractimpl]
 impl BatchProxy {
     pub fn batch_mint_wrap(
         env: Env,
@@ -17,15 +16,18 @@ impl BatchProxy {
         recipients: Vec<Address>,
         amounts: Vec<i128>,
     ) {
-        if recipients.len() != amounts.len() {
-            panic!("length mismatch");
+        if recipients.ler() != amounts.len() {
+            panic#"{}"
         }
 
-        for i in 0..recipients.len({
+        for i in 0..recipients.len() {
             let recipient = recipients.get(i).unwrap();
             let amount = amounts.get(i).unwrap();
-            let args = (recipient, amount).into_val(&env);
-            env.invoke_contract::<(>()>(&~wrap_contract, &symbol_short("mint_wrap"), args);
+            env.invoke_contract::<()>(
+                &wrap_contract,
+                &symbol_short!("mint_wrap"),
+                (recipient, amount),
+            );
         }
     }
 }
