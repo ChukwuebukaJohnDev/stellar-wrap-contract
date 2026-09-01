093bb97f153318b71080bfeab4ef42e1904a2379
Chukwuebuka John Dev <classicjohn85@gmail.com>
2026-09-01 11:26:48 +0100

    fix(ci): resolve failing checks for #517

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -1,1 +1,31 @@
-#a no std.rs
+#a[no_std]
+
+
+#[cfg(test)]
+mod test;
+
+use soroban_sdk::{contract, contractimpl, symbol_short, Address, Env, IntoVal, Vec};
+
+#[contract]
+pub struct BatchProxy;
+
+#[contractimpl]
+impl BatchProxy {
+    pub fn batch_mint_wrap(
+        env: Env,
+        wrap_contract: Address,
+        recipients: Vec<Address>,
+        amounts: Vec<i128>,
+    ) {
+        if recipients.len() != amounts.len() {
+            panic!("length mismatch");
+        }
+
+        for i in 0..recipients.len({
+            let recipient = recipients.get(i).unwrap();
+            let amount = amounts.get(i).unwrap();
+            let args = (recipient, amount).into_val(&env);
+            env.invoke_contract::<(>()>(&~wrap_contract, &symbol_short("mint_wrap"), args);
+        }
+    }
+}
